856b6fb2fbc8cc3c63ce5f2e88dd15af447b44f8
Mohd Shujaat Ali <32382909+shujaat786@users.noreply.github.com>
2024-11-28 01:36:31 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/auth', // Base URL of your backend API
-    timeout: 5000, // Set timeout for requests
+    baseURL: 'http://localhost:4000/auth',
+    timeout: 5000, 
     headers: {
         'Content-Type': 'application/json',
     },
